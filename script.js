d09d274830a8747d4f33d044bf18cc197dbf5edd
Gunjana Choudhary <choudharygunjana@gmail.com>
2025-05-14 13:31:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
 // your code here
+
+	let newarray = [0,1];
+	let first  = 1;
+
+	while(true){
+		if(newarray[newarray.length-1]===num){
+			return first;
+		}
+		newarray.push(first);
+		first += newarray[newarray.length-1];
+	}
+	
 }
 
 module.exports = fibonacci;
